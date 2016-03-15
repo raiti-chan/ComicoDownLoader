e4795569daf0039795e204ace835a8be4982222f
らいち <rusuke@t-net.ne.jp>
2016-03-15 21:52:44 +0900

    βのソースを消去

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,6 +1,4 @@
 package client;
-	
-import raiti.RaitisAPI.util.PrintStream.DualPrintStream;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -11,42 +9,13 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	
-	/**
-	 * 出力ストリーム
-	 */
-	public static DualPrintStream dps;
 	
+	/**
+	 * バージョン
+	 */
 	public static final String Ver = "1.0.0";
 	
-	/**
-	 * Comicoの漫画のトップページURL 後ろに作品ナンバーを付けて使用
-	 */
-	public static final String MAINPAGEURL = "http://www.comico.jp/articleList.nhn?titleNo=";
-	
-	/**
-	 * 漫画ページのURL 後ろに作品ナンバーとストーシーインデックスを付けます。
-	 */
-	public static final String COMICPAGEURL = "http://www.comico.jp/detail.nhn?titleNo=";
-	
-	/**
-	 * ストーリーインデックスのベースです。後ろにインデックスを付与してください
-	 */
-	public static final String STORYINDEXBASE = "&articleNo=";
-	
-	/**
-	 * ページインデックスタグ
-	 */
-	public static final String PAGEINDEXTAG ="&page=";
-	
-	/**
-	 * ヘッダー画像のURL
-	 */
-	public static final String HEADERURL ="http://images.comico.jp/up/r01/";
-	
-	/**
-	 * ヘッダー画像のURL2
-	 */
-	public static final String HEADERURL2 = "_kv1600x430.jpg";
+
 	
 	@Override
 	public void start(Stage primaryStage) {
